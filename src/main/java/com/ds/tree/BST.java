f56229809d4493eb144a7bef22b2eeb6a5da7c00
jayavardhanpatil <jayawardhan.patil@gmail.com>
2019-11-08 23:28:37 -0800

    Initial Commit

---
+++ src/main/java/com/ds/tree/BST.java
@@ -0,0 +1,12 @@
+package com.ds.tree;
+
+/**
+ * Created by jayavardhanpatil on 11/8/19
+ */
+public class BST {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/ds/tree/Node.java
@@ -0,0 +1,12 @@
+package com.ds.tree;
+
+/**
+ * Created by jayavardhanpatil on 11/8/19
+ */
+public class Node {
+
+    int data;
+    Node leftChild;
+    Node rightChild;
+
+}
